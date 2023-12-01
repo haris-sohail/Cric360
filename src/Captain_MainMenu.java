58e9b61ff5a7ff8ed80bba0e58c686f9dc76a4f1
Tallal <mmtallal@gmail.com>
2023-12-01 18:05:31 +0500

    LeaderBoard Done

--- src/Captain_MainMenu.java
+++ src/Captain_MainMenu.java
@@ -7,6 +7,7 @@ public class Captain_MainMenu extends JFrame{
     private JPanel panelMain;
     private JButton btnSeeRequests;
     private JButton writeBlogButton;
+    private JButton leaderboardbutton;
 
     public Captain_MainMenu() {
         btnSeeRequests.addActionListener(new ActionListener() {
@@ -35,6 +36,22 @@ public class Captain_MainMenu extends JFrame{
                 blog_obj.setLocationRelativeTo(null);
                 blog_obj.setVisible(true);
                 blog_obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                dispose();
+            }
+        });
+        leaderboardbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Show Leaderboard");
+                LeaderBoard leaderBoard_obj = new LeaderBoard();
+                leaderBoard_obj.setContentPane(leaderBoard_obj.getPanelMain());
+                leaderBoard_obj.setSize(400, 400);
+                leaderBoard_obj.setLocationRelativeTo(null);
+                leaderBoard_obj.setVisible(true);
+                leaderBoard_obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                dispose();
             }
         });
     }
