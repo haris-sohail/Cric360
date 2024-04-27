e18f303b5b2d72862426fb9c7a35336808faefec
Haris Sohail <harissohail.cs@gmail.com>
2024-04-27 18:00:12 +0500

    Add username to disucssion when posting to db
    It was not being added previously

--- client/src/components/StartDiscussionDetails.js
+++ client/src/components/StartDiscussionDetails.js
@@ -24,7 +24,7 @@ function StartDiscussionDetails() {
 
             // insert in database
             try {
-                const res = await axios.post('http://localhost:3001/postDiscussion', { title, text, comments, upvotes, downvotes })
+                const res = await axios.post('http://localhost:3001/postDiscussion', { title, text, comments, upvotes, downvotes, username })
 
                 if (res)
                     toast.success("Posted Successfully")
