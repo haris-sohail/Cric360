410e3d30e35559448303c516fc30f298e2be4b56
i210583 <129320147+i210583@users.noreply.github.com>
2024-05-01 21:48:35 +0500

    Teams page frontend 1

--- client/src/components/Team.js
+++ client/src/components/Team.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-function Team() {
+function Team({ captain_username, name,logo, location, matches_played, matches_won, matches_drawn, matches_lost, battingAvg }) {
   return (
-    <div>Team</div>
-  )
+    <div className='object-container'>
+      <div className='title-container'>
+        <h2>{name}</h2>
+        <h2>{location}</h2>
+      </div>
+
+      <div className='image-container'>
+          <img src={logo}></img>
+      </div>
+    </div>
+  );
 }
 
-export default Team
+export default Team;
--- client/src/components/Teams.js
+++ client/src/components/Teams.js
@@ -1,8 +1,10 @@
-import React from 'react'
+// Teams.js
+import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import Navbar from './Navbar'
-import Team from './Team'
-import '../css/Teams.css'
+import Navbar from './Navbar';
+import Team from './Team';
+import '../css/Teams.css';
+import defaultteam from '../system/assets/logo.png'
 
 function Teams() {
     const location = useLocation();
@@ -11,16 +13,33 @@ function Teams() {
     const navigate = useNavigate();
 
     const handleRegisterTeam = () => {
-        navigate('/registerTeam', { state: { username } })
-    }
-    return (
-        <div className='teams-container'>
-            <Navbar username={username} />
-            <button onClick={handleRegisterTeam}><h6>Register a team</h6></button>
+        navigate('/registerTeam', { state: { username } });
+    };
 
-            <Team />
+    return (
+        <div className='main-div'>
+        <Navbar username={username} />
+        <div className='button-container'>
+        <button className='register-button' onClick={handleRegisterTeam}><h6>Register a team</h6></button>
         </div>
-    )
+        <div className='teams-container'>
+            <div className='object-container'>
+                <Team 
+                    captain_username={"Nigga"}
+                    name={"Niggas"}
+                    logo={defaultteam}
+                    location={"faislabad"}
+                    matches_played={10}
+                    matches_won={5}
+                    matches_drawn={2}
+                    matches_lost={3}
+                    battingAvg={10}
+                />
+               
+            </div>
+        </div>
+        </div>
+    );
 }
 
-export default Teams
+export default Teams;
