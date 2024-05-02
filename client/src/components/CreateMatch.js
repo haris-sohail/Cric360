7b1b220ac1054a1567bfaad8755788ca007d71e1
Haris Sohail <harissohail.cs@gmail.com>
2024-05-02 16:29:30 +0500

    Add front-end to create match component

--- client/src/components/CreateMatch.js
+++ client/src/components/CreateMatch.js
@@ -1,7 +1,17 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import '../css/CreateMatch.css'
 import Navbar from './Navbar';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import HeadingMessage from './HeadingMessage';
 
 function CreateMatch() {
     const location = useLocation();
@@ -11,15 +21,43 @@ function CreateMatch() {
     const [startingAt, setStartinAt] = useState()
     const [venue, setVenue] = useState()
     const [format, setFormat] = useState()
-    const [teamA, setTeamA] = useState()
 
     return (
         <div className='create-match-container'>
             <Navbar username={username} />
 
-            <h1>Creating a Match</h1>
-            <div className='text-input-create-match'>
-                
+            <div className='match-input-create-match'>
+                <HeadingMessage message={"Creating Match"} id='heading-message-create-match' />
+
+                <FormControl variant="standard" fullWidth>
+                    <InputLabel id="demo-simple-select-standard-label"><h6>FORMAT</h6></InputLabel>
+                    <Select
+                        labelId="demo-simple-select-standard-label"
+                        id="demo-simple-select-standard"
+                        value={format}
+                        onChange={(e) => { setFormat(e.target.value) }}
+                        label="format"
+                    >
+                        <MenuItem value={"odi"}><h6>ODI</h6></MenuItem>
+                        <MenuItem value={"t20"}><h6>T20</h6></MenuItem>
+                        <MenuItem value={"test"}><h6>TEST</h6></MenuItem>
+                    </Select>
+                </FormControl>
+
+                <input placeholder='VENUE' onChange={(e) => { setVenue(e.target.value.trim()) }} id='venue-in-create-match'></input>
+
+
+                <LocalizationProvider dateAdapter={AdapterDayjs} className='date-time-container-create-match'>
+                    <DemoContainer components={['DateTimePicker']}>
+                        <DateTimePicker
+                            label="Starting at"
+                        />
+                    </DemoContainer>
+                </LocalizationProvider>
+
+                <div id='create-button-container-create-match'>
+                    <button><h6>Create</h6></button>
+                </div>
             </div>
         </div>
     )
