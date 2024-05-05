a3dc7397d03b1ade315a652cfd3d440e178e8390
Haris Sohail <harissohail.cs@gmail.com>
2024-05-05 15:52:08 +0500

    Update front-end of match handler, total stats now showing

--- client/src/components/Innings.js
+++ client/src/components/Innings.js
@@ -5,11 +5,18 @@ import BowlingInnings from './BowlingInnings'
 import ScoreMachine from './ScoreMachine'
 
 function Innings({ matchStatsID, battingTeam, bowlingTeam }) {
+    const [totalRuns, setTotalRuns] = useState(0)
+    const [wickets, setWickets] = useState(0)
+    const [overs, setOvers] = useState(0.0)
+
     useEffect(() => {
         console.log(battingTeam, bowlingTeam)
     })
     return (
         <div className='innings-container'>
+            <div className='inning-stats-container-inning'>
+                <h6>{totalRuns}/{wickets} ({overs})</h6>
+            </div>
             <div className='batsmen-innings-container-innings'>
                 <BatsmanInnings battingTeam={battingTeam} />
                 <BatsmanInnings battingTeam={battingTeam} />
