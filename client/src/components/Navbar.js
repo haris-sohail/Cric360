b9b0c979fc96c217c9662760f0c0d48950c255b3
Haris Sohail <harissohail.cs@gmail.com>
2024-05-08 15:21:08 +0500

    Revert navbar changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,75 +1,44 @@
-import React, { useState } from "react";
-import Logo from "../system/assets/logo.png";
-import { Link, useNavigate } from "react-router-dom";
-import "../css/Navbar.css";
+import React from 'react'
+import Logo from '../system/assets/logo.png'
+import { Link, useNavigate } from 'react-router-dom';
+import '../css/Navbar.css'
 
 function Navbar({ username }) {
     const navigate = useNavigate();
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     const handleLogoClick = () => {
-        navigate("/home", { state: { username } });
-    };
+        navigate('/home', { state: { username } })
+    }
 
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
+    const handleTeamsClick = () => {
+        navigate('/teams', { state: { username } })
+    }
+
+    const handleMatchesClick = () => {
+        navigate('/matches', { state: { username } })
+    }
 
     return (
-        <>
-            <div className="Overall-Nav">
-                <div className="navbar">
-                    <a onClick={handleLogoClick} id="logo-link">
-                        <img src={Logo} alt="logo" />
-                    </a>
+        <div className='navbar'>
+            <a onClick={handleLogoClick} id='logo-link'>
+                <img src={Logo} alt="logo" />
+            </a>
 
-                    <div className="btns-container">
-                        <ul>
-                            <li>
-                                <Link>
-                                    <button>
-                                        <h6>Matches</h6>
-                                    </button>
-                                </Link>
-                            </li>
-                        </ul>
-                        <ul>
-                            <li>
-                                <Link>
-                                    <button>
-                                        <h6>Stats</h6>
-                                    </button>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+            <div className='btns-container'>
+                <a onClick={handleMatchesClick}>
+                    <button><h6>Matches</h6></button>
+                </a>
 
-                {/* Sidebar */}
-                <div className={isSidebarOpen ? "sidebar show" : "sidebar"}>
-                    <ul>
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">About</a>
-                        </li>
-                        <li>
-                            <a href="#">Services </a>
-                        </li>
-                        <li>
-                            <a href="#">Contact</a>
-                        </li>
-                    </ul>
-                </div>
+                <Link>
+                    <button><h6>Stats</h6></button>
+                </Link>
 
-                {/* Toggle button */}
-                <button className="toggle-btn" onClick={toggleSidebar}>
-                    &#9776;
-                </button>
+                <a onClick={handleTeamsClick}>
+                    <button><h6>Teams</h6></button>
+                </a>
             </div>
-        </>
-    );
+        </div>
+    )
 }
 
-export default Navbar;
+export default Navbar
