1a84cab3ce94148ada8a832ed9eac8c30a327f78
Haris Sohail <harissohail.cs@gmail.com>
2024-05-11 20:19:06 +0500

    Add a welcome message for api - fix

--- server/server.js
+++ server/server.js
@@ -13,13 +13,14 @@ app.use(cors(
   }
 ))
 
-app.get('/', (req, res) => {
-  res.json("Welcome to Cric360-api")
-})
 
 app.use(express.static('public'))
 
 mongoose.connect(process.env.MONGODB_URI);
+
+app.get('/', (req, res) => {
+  res.json("Welcome to Cric360-api")
+})
 
 const userRoutes = require('./routes/userRoutes');
 const discussionRoutes = require('./routes/discussionRoutes');
