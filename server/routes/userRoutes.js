c51c832ecbc2a05a672a534bf79ec51136639fc6
Haris Sohail <harissohail.cs@gmail.com>
2024-05-11 21:32:53 +0500

    Testing deploymnt

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -21,6 +21,7 @@ router.post('/getEmail', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
+  console.log(req.body)
   UserModel.findOne({ username: req.body.username, password: req.body.password })
     .then(user => res.json(user))
     .catch(err => res.json(err))
