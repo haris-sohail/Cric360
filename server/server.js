3308b64681d7fbaf3cec7cb9596b30f0b72e74f8
Haris Sohail <harissohail.cs@gmail.com>
2024-05-11 20:01:44 +0500

    Add origin url in server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
   {
-    origin: [""],
+    origin: ["https://cric360.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
   }
