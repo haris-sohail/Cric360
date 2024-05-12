b6f4a34bd3746b2716dcd8881d307d93cb306145
Haris Sohail <harissohail.cs@gmail.com>
2024-05-12 11:43:31 +0500

    Add user model in server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 require('dotenv').config();
+const UserModel = require("../Models/User");
 
 const app = express()
 app.use(express.json())
